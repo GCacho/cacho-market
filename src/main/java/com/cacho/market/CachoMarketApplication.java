f49b7a04fadf4165a1acd1e5286175ed5a557f36
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2023-10-17 11:59:34 -0700

    first commit

---
+++ src/main/java/com/cacho/market/CachoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.cacho.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cacho/market/CachoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacho.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
